e8564e0763943ce4c00428faef722f5f7273d7a5
thomas_huchede <huchede.thomas@gmail.com>
2016-06-02 09:45:37 +0200

    fix lancher

--- server.py
+++ server.py
@@ -1,6 +1,10 @@
 from flask import Flask, url_for, send_file, request
-import os
+import subprocess
 app = Flask(__name__)
+
+@app.route('/')
+def root():
+    return 'root'
 
 @app.route('/create', methods=['POST'])
 def create():
@@ -10,8 +14,8 @@ def create():
         longitude = data['home']['geopoint']['longitude']
         latitude = data['home']['geopoint']['latitude']
         altitude = data['home']['geopoint']['altitude']
-        os.system('dronekit-sitl copter --home=' + str(latitude) + ','
-        + str(longitude) + ',' + str(altitude) + ',1 --instance=' + str(instance))
+        subprocess.Popen(['/home/adra/.local/bin/dronekit-sitl',  'copter',  '--home=' + str(latitude) + ','
+        + str(longitude) + ',' + str(altitude) + ',1 ', '--instance=' + str(instance)])
         return 'OK', 200
     return 'Error', 404
 
@@ -22,22 +26,40 @@ def mission():
         ident = data['id']
         intervention = data['intervention']
         instance = data['instance']
-        geopoints = data['mission']['geopoints']
+        print str(ident)
+        print str(intervention)
+        print str(instance)
+        print data['mission']['mission']
+        geopoints = data['mission']['mission']['geopoints']
         mission = ''
+        argg = ['python', 'mission.py', '--id', str(ident),
+        '--intervention', str(intervention), '--instance', str(instance),
+        '--points']
         for point in range(len(geopoints)):
-            mission += str(geopoints[point]['latitude'])
-            mission += ' '
-            mission += str(geopoints[point]['longitude'])
-            mission += ' '
-            mission += str(geopoints[point]['altitude'])
-            mission += ' '
+            """
+            argg.append(geopoints[point]['latitude'])
+            argg.append(geopoints[point]['longitude'])
+            argg.append(geopoints[point]['altitude'])
+            """
+            argg.append(str(geopoints[point]['latitude']))
+            argg.append(str(geopoints[point]['longitude']))
+            argg.append(str(geopoints[point]['altitude']))
+
         print mission
-        os.system('python mission.py --id ' + str(ident)
-        + ' --intervention ' + str(intervention) + ' --instance' + str(instance)
-        + ' --mission' + str(mission))
+        subprocess.Popen(argg)
         return 'OK', 200
     return 'Error', 404
 
+"""
+
+mission += str(geopoints[point]['latitude'])
+mission += ' '
+mission += str(geopoints[point]['longitude'])
+mission += ' '
+mission += str(geopoints[point]['altitude'])
+if point != len(geopoints)-1:
+    mission += ' '
+"""
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
